6c842ff8d1792b856724cf13782513b7408faf46
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2026-01-13 19:09:42 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.07 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        StringBuilder buffer = new StringBuilder();
+        
+        for(int i=0; i<myString.length(); i++){
+            char c = myString.charAt(i);
+            
+            if(c == 'A'){
+                buffer.append('B');
+            }else{
+                buffer.append('A');
+            }
+        }
+        
+        if(buffer.toString().contains(pat)){
+            answer = 1;
+        }
+        
+        return answer;
+    }
+}
