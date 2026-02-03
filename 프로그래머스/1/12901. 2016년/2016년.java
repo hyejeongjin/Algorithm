fd1552486846b1308ec51dc6018bf3bd52dab47d
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2026-02-03 20:48:42 +0900

    [level 1] Title: 2016년, Time: 109.96 ms, Memory: 94.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+import java.time.DayOfWeek;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        
+        LocalDate date = LocalDate.of(2016, a, b);
+        answer = date.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.US).toUpperCase();
+        
+        return answer;
+    }
+}
